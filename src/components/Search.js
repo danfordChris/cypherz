93ea46bb64686ab550d146a3e7c3f8c29bd20eb6
insypher1~ <insyphercodes@gmail.com>
2023-11-26 13:05:01 +0300

    whatever-2

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,36 +1,34 @@
-import React from 'react'
+import React from "react";
 
 function Search() {
   return (
     <div>
       <div class="p-3 mb-3 w-96">
-  <div class="relative mb-4 flex w-full flex-wrap items-stretch">
-    <input
-      type="search"
-      class="relative m-0 block min-w-0 flex-auto rounded border border-solid border-green-800 bg-green-200 bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-black dark:focus:border-primary"
-      placeholder="Search products"
-      aria-label="Search"
-      aria-describedby="button-addon2" />
+        <div class="relative mb-4 flex w-full flex-wrap items-stretch">
+          <input
+            type="search"
+            class="relative m-0 block min-w-0 flex-auto rounded border border-solid border-green-800 bg-white bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-black dark:focus:border-primary"
+            placeholder="Search products"
+            aria-label="Search"
+            aria-describedby="button-addon2"
+          />
 
-  
-    <span
-      class="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-700 dark:text-black"
-      id="basic-addon2">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        class="h-5 w-5">
-        <path
-          fill-rule="evenodd"
-          d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-          clip-rule="evenodd" />
-      </svg>
-    </span>
-  </div>
-</div>
+          <span
+            class="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-700 dark:text-black"
+            id="basic-addon2"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
+              <path
+                fill-rule="evenodd"
+                d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
