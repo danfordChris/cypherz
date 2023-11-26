d8d4e8628dd6ed5056323f4c340fef9fcdc910f1
insypher1~ <insyphercodes@gmail.com>
2023-11-26 13:03:00 +0300

    whatever

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 // Import necessary components and modules
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
-import Home from './components/LandingPage';
-import Services from './components/Services';
-import Offers from './components/Darshbord';
-import About from './components/About';
-import Profile from './components/profile/Profile';
-import Login from './components/login/Login';
-import Register from './components/register/register';
-
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/LandingPage";
+import Services from "./components/Services";
+import Offers from "./components/Darshbord";
+import About from "./components/About";
+import Profile from "./components/Profile/Profile";
+import Login from "./components/login/Login";
+import Register from "./components/register/register";
 
 function App() {
   return (
@@ -19,9 +17,9 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
-          <Route path="/Login" element={<Login />} /> 
-          <Route path="/register" element={<Register />} /> 
-          <Route path="/profile" element={<Profile />} /> 
+          <Route path="/Login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="/services" element={<Services />} />
           <Route path="/offers" element={<Offers />} />
           <Route path="/about" element={<About />} />
--- src/components/Darshbord.js
+++ src/components/Darshbord.js
@@ -1,67 +1,69 @@
-import React from 'react'
+import React from "react";
+import "./Dashboard.css";
 
-import Search from './Search'
-import PaymentDetails from './PaymentDetails'
+import Search from "./Search";
+import PaymentDetails from "./PaymentDetails";
 
 function Darshbord() {
   return (
     <>
-   
-    <Search />
-    <div className="flex justify-left items-left h-screen">
-      <div className="relative overflow-x-auto">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Product name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Color
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Category
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Price
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Apple MacBook Pro 17"
-              </th>
-              <td className="px-6 py-4">Silver</td>
-              <td className="px-6 py-4">Laptop</td>
-              <td className="px-6 py-4">$2999</td>
-            </tr>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Microsoft Surface Pro
-              </th>
-              <td className="px-6 py-4">White</td>
-              <td className="px-6 py-4">Laptop PC</td>
-              <td className="px-6 py-4">$1999</td>
-            </tr>
-            <tr className="bg-white dark:bg-gray-800">
-              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Magic Mouse 2
-              </th>
-              <td className="px-6 py-4">Black</td>
-              <td className="px-6 py-4">Accessories</td>
-              <td className="px-6 py-4">$99</td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="dashboardPage">
+        <div className="searchProduct">
+          <Search />
+          <div className="flex justify-left items-left h-screen">
+            <div className="relative overflow-x-auto">
+              <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-black uppercase bg-gray-50 dark:bg-green-800 dark:text-gray-400">
+                  <tr>
+                    <th scope="col" className="text-white px-6 py-3">
+                      Product name
+                    </th>
+                    <th scope="col" className="text-white px-6 py-3">
+                      Amount /kg
+                    </th>
+                    <th scope="col" className="text-white px-6 py-3">
+                      Category
+                    </th>
+                    <th scope="col" className="text-white px-6 py-3">
+                      Price /Tsh
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="bg-white border-b dark:bg-green-700 dark:border-green-800">
+                    <th scope="row" className="px-6 py-4 font-medium text-white whitespace-nowrap dark:text-white">
+                      Maize
+                    </th>
+                    <td className="text-white px-6 py-4">100</td>
+                    <td className="text-white px-6 py-4">Food</td>
+                    <td className="text-white px-6 py-4">300,000</td>
+                  </tr>
+                  <tr className="bg-white border-b dark:bg-green-700 dark:border-gray-700">
+                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                      Beans
+                    </th>
+                    <td className="text-white px-6 py-4">75</td>
+                    <td className="text-white px-6 py-4">Food</td>
+                    <td className="text-white px-6 py-4">225,000</td>
+                  </tr>
+                  <tr className="bg-white border-b dark:bg-green-700 dark:border-gray-700">
+                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                      Mangoes
+                    </th>
+                    <td className="text-white px-6 py-4">50</td>
+                    <td className="text-white px-6 py-4">Fruits</td>
+                    <td className="text-white px-6 py-4">125,000</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+
+        <PaymentDetails />
       </div>
-    </div>
-
-    
-    <PaymentDetails />
-
     </>
-  )
+  );
 }
 
-export default Darshbord
+export default Darshbord;
